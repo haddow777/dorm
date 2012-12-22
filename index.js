2425bfcf9732d2869433ed4344349308fd12a729
Daniel Werner <dan.werner@gmail.com>
2012-12-21 21:09:42 -0800

    Adding working version

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var dorm = require('./lib/dorm')
+  , Fields = require('./lib/Fields')
+  , Entity = require('./lib/Entity');
+  
+dorm.Fields = Fields;
+dorm.Entity = Entity;
+
+module.exports = dorm;
