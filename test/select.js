68976a2a71b9371b640985486a801fab18f29d57
dan <dan@beastly.(none)>
2013-01-30 16:39:02 -0800

    fixing select * scenario

--- test/select.js
+++ test/select.js
@@ -3,18 +3,6 @@ var dorm = require('../lib/dorm')
   , util = require('util');
 
 module.exports["test: select UserRole"] = function (beforeExit, assert){
-	/*var handler = function(err, res){
-		
-	};
-	  
-	dorm.get(e.UserRole, {
-	        where:{
-	            name:{
-	              cmp:'=', value:'admin'
-	            }
-	        }
-	    }, handler);
-	    
-	dorm.get(e.UserRole, 1, handler);*/
+	dorm.get(e.UserRole, {});
 };
 
